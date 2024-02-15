6c1ff3fd7708eb20447743692251093107d3d1fb
hsaglam <huseyin.saglam@etiya.com>
2024-02-15 23:26:29 +0300

    Initial commit

---
+++ src/test/java/com/example/spring/data/jpa/test/SpringBootDatajpatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.data.jpa.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDatajpatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
